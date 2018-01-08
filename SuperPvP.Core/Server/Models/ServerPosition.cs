a5d4fa13e696a3279872c1c79306e145c3c227cf
mozgovoy <mozgovoy@playkot.com>
2018-01-08 14:00:33 +0300

    + update/command packet bug

--- SuperPvP.Core/Server/Models/ServerPosition.cs
+++ SuperPvP.Core/Server/Models/ServerPosition.cs
@@ -1,4 +1,7 @@
-﻿namespace SuperPvP.Core.Server.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperPvP.Core.Server.Models
 {
     public class ServerPosition
     {
@@ -14,6 +17,46 @@
         {
             I = i;
             J = j;
+        }
+
+        public static bool IsNullOrEmpty(ServerPosition p)
+        {
+            return p == null || (p.I == 0 && p.J == 0);
+        }
+
+        public static ServerPosition operator - (ServerPosition begin, ServerPosition end)
+        {
+            return new ServerPosition
+            {
+                I = end.I - begin.I,
+                J = end.J - begin.J
+            };
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            var sp = (ServerPosition) obj;
+            return I == sp.I && J == sp.J;
+        }
+
+        public void AddVector (ServerPosition p)
+        {
+            I += p.I;
+            J += p.J;
+        }
+
+        public ServerPosition Normalize()
+        {
+            int i = I == 0 ? 0 : Math.Sign(I);
+            int j = J == 0 ? 0 : Math.Sign(J);
+
+            return new ServerPosition(i, j);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", I, J);
         }
     }
 }
